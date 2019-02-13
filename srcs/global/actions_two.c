e4fa9c7dbd2a42f90630e44aa584b66190d3bb62
Arthur Dassier <arthur.dassier@epitech.eu>
2019-02-13 13:42:39 +0100

    good sorting

--- srcs/global/actions_two.c
+++ srcs/global/actions_two.c
@@ -13,13 +13,17 @@ int my_strcmp(char const *s1, char const *s2)
     int i = 0;
     int j = 0;
 
-    while(s1[i] == '_' && s1[i])
+    while(s1[i] == '_' || s1[i] == '.' && s1[i])
         ++i;
-    while(s2[j] == '_' && s2[j])
+    while(s2[j] == '_' || s2[j] == '_' && s2[j])
         ++j;
     while (tolower(s1[i]) == tolower(s2[j]) && s1[i] && s2[j]) {
         ++j;
         ++i;
+        while(!(isalnum(s1[i])) && s1[i])
+            ++i;
+        while(!(isalnum(s2[j])) && s2[j])
+            ++j;
     }
     if ((s1[i] == '\0' && s2[j] == '\0')) {
         if (s1[0] == '_')
