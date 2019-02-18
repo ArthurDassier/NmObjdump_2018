2276d44bee75d46cd280b51dab03098c57e80625
Arthur Dassier <arthur.dassier@epitech.eu>
2019-02-18 16:26:14 +0100

    deleetd debug

--- srcs/nm/global/print.c
+++ srcs/nm/global/print.c
@@ -22,5 +22,4 @@ void print_me_that(chainlist *list)
         printf("%016x %c %s\n", list->adress, list->type, list->name);
     else
         printf("\t\t %c %s\n", list->type, list->name);
-    printf("%d : \n", a);
 }
